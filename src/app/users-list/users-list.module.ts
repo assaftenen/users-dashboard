763fda8ad38a55142e4afaff67aea28ce6c5a0f6
assaf ten <assaften@artlist.io>
2020-02-20 21:11:44 +0200

    css | user | users - list module | router

---
+++ src/app/users-list/users-list.module.ts
@@ -0,0 +1,8 @@
+import { NgModule } from '@angular/core';
+import { UsersDashboardComponent } from './users-dashboard/users-dashboard.component';
+@NgModule({
+  declarations: [UsersDashboardComponent],
+  imports: [
+  ]
+})
+export class UsersListModule { }
